a0c515fdd27fa35e8bd684ca067a3438bbfe45ec
adhith19 <adhithdinesh06@gmail.com>
2025-12-14 12:50:42 +0530

    Change database password in DBConnection
    Updated the database password in DBConnection.java.

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -7,9 +7,10 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/food_ordering";
     private static final String USER = "root";
-    private static final String PASSWORD = "adhith@@1011";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
 }
